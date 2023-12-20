8cbf5382c020cb02ce5391fe35abab6d163cc562
49rip <146723162+49rip@users.noreply.github.com>
2023-12-20 20:31:39 +0300

    Update msgsendingpage.js

--- msgsendingpage.js
+++ msgsendingpage.js
@@ -1,44 +1,4 @@
 
-<script>
-document.addEventListener('contextmenu', event => event.preventDefault());
-</script>
-<!DOCTYPE html>
-<html>
-<head>
-	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script>
-		$(document).ready(function(){
-			$("#send-btn").click(function(){
-				var message = $("#message-input").val();
-				var data = {
-					"content": message
-				};
-				$.ajax({
-					type: "POST",
-					url: "https://discord.com/api/webhooks/1187084100083916912/stZujhaM77-lwi35EtkHl13MY7SwLNFFPW843pSW4bycS5QMlQPun6wYBnkaDc_Q2fYp",
-					data: JSON.stringify(data),
-					contentType: "application/json",
-					success: function(){
-						alert("Message sent successfully!");
-					},
-					error: function(){
-						alert("Message sending failed. Please try again later.");
-					}
-				});
-			});
-		});
-	</script>
-</head>
-<body>
-	
-	<p>Enter your message below:</p>
-	<textarea id="message-input"></textarea>
-	<br /><br />
-	<button id="send-btn">Send Message</button>
-</body>
-</html>
-
 
 
 <!DOCTYPE html>
